07a856ad1812f707c1bf4457cdd204844adb56d7
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-02-13 13:32:19 -0800

    feat(frontend): Add logo placeholder
    Added logo placeholder for landing page.

--- apps/frontend/src/components/LandingPage.tsx
+++ apps/frontend/src/components/LandingPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, ButtonGroup, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Image } from "@chakra-ui/react";
 
 interface LandingPageProps {
   children: React.ReactNode;
@@ -11,9 +11,25 @@ function LandingPage() {
   return (
     <>
       {/* General container for the page */}
-      <Box w={"100vw"} h={"100vh"} bg={"orange"}>
+      {/* Relative position of parent box ensures the children within it
+      are position relative to the parent, not the viewport. */}
+      <Box w={"100vw"} h={"100vh"} bg={"orange"} position={"relative"}>
         {/* Logo */}
-        <Flex border={"black solid 2px"} justifyContent={"center"}></Flex>
+        <Flex
+          position={"absolute"}
+          top={"33%"}
+          left={0}
+          right={0}
+          justifyContent={"space-evenly"}
+        >
+          <Box border={"black solid 2px"} justifyContent={"space-evenly"}>
+            <Image
+              src={"https://via.placeholder.com/200"}
+              alt={"placeholder"}
+              boxSize={"200px"}
+            />
+          </Box>
+        </Flex>
 
         {/* Buttons */}
         <Flex
@@ -23,10 +39,10 @@ function LandingPage() {
           right={0} // Align to right
           justifyContent={"space-evenly"} // Ensures buttons are evenly spaced
         >
-          <Button width={"45%"} height={"3em"} paddingY={2}>
+          <Button width={"45%"} height={"4em"} paddingY={2}>
             REGISTER
           </Button>
-          <Button width={"45%"} height={"3em"} paddingY={2}>
+          <Button width={"45%"} height={"4em"} paddingY={2}>
             SIGN UP
           </Button>
         </Flex>
--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./global.styles.scss";
-import Registration from "./components/RegistrationPage";
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 
