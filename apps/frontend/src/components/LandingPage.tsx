3082a57189c2ad002436b24e9b01997f294f3d08
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-02-13 18:54:04 -0800

    refactor: Simplify LandingPage.tsx code
    Split the page into two components for readability.

--- apps/frontend/src/components/LandingPage.tsx
+++ apps/frontend/src/components/LandingPage.tsx
@@ -1,53 +1,49 @@
 import React from "react";
-import { Box, Button, Flex, Image } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, VStack } from "@chakra-ui/react";
 
 interface LandingPageProps {
   children: React.ReactNode;
+}
+
+function AuthButtons() {
+  const buttonStyles = {
+    width: "50%",
+    height: "4em",
+  };
+  return (
+    <VStack w={"100%"} p={"10px 0"}>
+      <Button width={buttonStyles.width} height={buttonStyles.height}>
+        SIGN UP
+      </Button>
+      <Button width={buttonStyles.width} height={buttonStyles.height}>
+        LOG IN
+      </Button>
+    </VStack>
+  );
 }
 
 /* Children is a special prop that allows a component
 to have nested components. */
 function LandingPage() {
   return (
-    <>
-      {/* General container for the page */}
-      {/* Relative position of parent box ensures the children within it
-      are position relative to the parent, not the viewport. */}
-      <Box w={"100vw"} h={"100vh"} bg={"orange"} position={"relative"}>
-        {/* Logo */}
-        <Flex
-          position={"absolute"}
-          top={"33%"}
-          left={0}
-          right={0}
-          justifyContent={"space-evenly"}
-        >
-          <Box border={"black solid 2px"} justifyContent={"space-evenly"}>
-            <Image
-              src={"https://via.placeholder.com/200"}
-              alt={"placeholder"}
-              boxSize={"200px"}
-            />
-          </Box>
-        </Flex>
-
-        {/* Buttons */}
-        <Flex
-          position={"absolute"}
-          top={"66%"} // Places box 2/3 down page
-          left={0} // Align to left
-          right={0} // Align to right
-          justifyContent={"space-evenly"} // Ensures buttons are evenly spaced
-        >
-          <Button width={"45%"} height={"4em"} paddingY={2}>
-            REGISTER
-          </Button>
-          <Button width={"45%"} height={"4em"} paddingY={2}>
-            SIGN UP
-          </Button>
-        </Flex>
+    <Flex
+      w={"100vw"}
+      h={"100vh"}
+      bg={"orange"}
+      align={"center"}
+      justify={"center"}
+      direction={"column"}
+    >
+      {/* Logo */}
+      <Box border={"black 2px solid"}>
+        <Image
+          src={"https://via.placeholder.com/200"}
+          alt={"Company Logog Placeholder"}
+          boxSize={"250px"}
+        />
       </Box>
-    </>
+      <AuthButtons />
+    </Flex>
   );
 }
 
