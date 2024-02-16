061d676fe56d53ffb677fa152275fc92013ccf74
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-02-15 20:06:08 -0800

    refactor(frontent): create `LoginForm` component
    Create a login form; add prop to `LoginPage` to accept LoginForm when rendered; removed redundant imports.

---
+++ apps/frontend/src/components/LoginForm.tsx
@@ -0,0 +1,20 @@
+import { Button, FormControl, Input, Stack } from "@chakra-ui/react";
+import React from "react";
+
+function LoginForm() {
+    return (
+        <Stack spacing={8} w={{ base: "100%", md: "80%", lg: "50%" }}>
+            <FormControl id="email">
+                <Input type="email" placeholder="Email" />
+            </FormControl>
+            <FormControl id="password">
+                <Input type="password" placeholder="Password" />
+            </FormControl>
+            <Button bg={"brand.500"} variant="solid" mt={8} p={6}>
+                LOGIN
+            </Button>
+        </Stack>
+    );
+}
+
+export default LoginForm;
--- apps/frontend/src/components/LoginPage.tsx
+++ apps/frontend/src/components/LoginPage.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import {
   Box,
-  Button,
   Flex,
-  FormControl,
-  Input,
-  Stack,
   Heading,
   VStack,
 } from "@chakra-ui/react";
 import { HamburgerIcon, ChevronLeftIcon } from "@chakra-ui/icons";
 
+interface LoginPageProps {
+  children: React.ReactNode;
+}
+
 // Add Header component created by Toco following rebase / merge
-function LoginPage() {
+function LoginPage( { children }: LoginPageProps) {
   return (
       <Box w={"100vw"} h={"100vh"} bg={"white"}>
         <Box as="header" bg={"brand.500"} p={6} zIndex="sticky" top={0}>
@@ -27,17 +27,8 @@ function LoginPage() {
               Login
             </Heading>
           </Flex>
-          <Stack spacing={8} w={{ base: "100%", md: "80%", lg: "50%" }}>
-            <FormControl id="email">
-              <Input type="email" placeholder="Email" />
-            </FormControl>
-            <FormControl id="password">
-              <Input type="password" placeholder="Password" />
-            </FormControl>
-            <Button bg={"brand.500"} variant="solid" mt={8} p={6}>
-              LOGIN
-            </Button>
-          </Stack>
+          {/* Below is where the Login Form will be rendered. */}
+          {children}
         </VStack>
       </Box>
   );
