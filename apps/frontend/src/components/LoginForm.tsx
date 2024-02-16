705dc621fb94fcbb90b3646be1009587e37aeff8
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-02-16 14:12:10 -0800

    refactor(frontend): replace Stack with VStack

--- apps/frontend/src/components/LoginForm.tsx
+++ apps/frontend/src/components/LoginForm.tsx
@@ -1,9 +1,9 @@
-import { Button, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+import { Button, FormControl, FormLabel, Input, VStack } from "@chakra-ui/react";
 import React from "react";
 
 function LoginForm() {
     return (
-        <Stack spacing={8} w={{ base: "100%", md: "80%", lg: "50%" }}>
+        <VStack spacing={8} w={{ base: "100%", md: "80%", lg: "50%" }}>
             <FormControl id="email">
                 <FormLabel>Email</FormLabel>
                 <Input type="email" />
@@ -15,7 +15,7 @@ function LoginForm() {
             <Button bg={"brand.500"} color={"white"} _hover={{ bg: "brand.600" }} variant="solid" p={6}>
                 LOGIN
             </Button>
-        </Stack>
+        </VStack>
     );
 }
 
