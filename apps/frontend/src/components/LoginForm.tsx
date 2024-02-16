d347c557ececa669fd0d498b82a2a71cf25dfbdd
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-02-15 20:11:10 -0800

    refactor(frontend): add form labels; remove placeholders

--- apps/frontend/src/components/LoginForm.tsx
+++ apps/frontend/src/components/LoginForm.tsx
@@ -1,14 +1,16 @@
-import { Button, FormControl, Input, Stack } from "@chakra-ui/react";
+import { Button, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
 import React from "react";
 
 function LoginForm() {
     return (
         <Stack spacing={8} w={{ base: "100%", md: "80%", lg: "50%" }}>
             <FormControl id="email">
-                <Input type="email" placeholder="Email" />
+                <FormLabel>Email</FormLabel>
+                <Input type="email" />
             </FormControl>
             <FormControl id="password">
-                <Input type="password" placeholder="Password" />
+                <FormLabel>Password</FormLabel>
+                <Input type="password" />
             </FormControl>
             <Button bg={"brand.500"} color={"white"} _hover={{ bg: "brand.600" }} variant="solid" p={6}>
                 LOGIN
