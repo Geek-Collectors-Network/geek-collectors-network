53b08a64a1d76518a1465c35394f03b19c450c2d
Terence Grigoruk <terence.grigoruk@gmail.com>
2024-02-18 12:19:49 -0800

    fix(db): varchar to varbinary

--- apps/api/src/models/user.ts
+++ apps/api/src/models/user.ts
@@ -9,14 +9,15 @@ import {
 
 import { v4 as uuidv4 } from 'uuid';
 
-
 export const user = mysqlTable('user', {
-  id: varchar('id', { length: 128 }).primaryKey().$defaultFn(() => uuidv4()),
-  createdAt: datetime('createdAt').notNull(),
+  id: varchar('id', { length: 128 })
+    .primaryKey()
+    .$defaultFn(() => uuidv4()),
+  createdAt: datetime('createdAt'),
   updatedAt: datetime('updatedAt'),
   email: varchar('email', { length: 255 }).unique(),
-  hashedPassword: varchar('hashedPassword', { length: 255 }),
-  salt: varbinary('salt', { length: 16 }),
+  hashedPassword: varbinary('hashedPassword', { length: 256 }),
+  salt: varbinary('salt', { length: 32 }),
   firstName: varchar('firstName', { length: 20 }),
   lastName: varchar('lastName', { length: 20 }),
   displayName: varchar('username', { length: 20 }).unique(),
