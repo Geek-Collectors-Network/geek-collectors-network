575b5b6106ce0ceb4589a683fd4a24aec03b4433
toco <ttachibana@my.bcit.ca>
2024-02-19 02:30:18 -0800

    feat(frontend): add AuthProvider context

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from 'react-auth-kit';
+import createStore from 'react-auth-kit/createStore';
 import App from './App';
 import theme from './theme/theme';
+
+const store = createStore({
+  authType: 'cookie',
+  authName: '_auth',
+  cookieDomain: window.location.hostname,
+  cookieSecure: false, // set to true if served over HTTPS
+});
 
 const container = document.getElementById('__react_app__');
 const root = createRoot(container!);
 root.render(<React.StrictMode>
   <ChakraProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </ChakraProvider>
 </React.StrictMode>);
