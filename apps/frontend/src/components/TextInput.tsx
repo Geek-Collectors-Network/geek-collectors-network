9b690d62ac9183e885f7d5efdcf9d5687186fb00
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-02-22 11:23:50 -0800

    docs(frontend): add comments to explain functions

--- apps/frontend/src/components/TextInput.tsx
+++ apps/frontend/src/components/TextInput.tsx
@@ -9,9 +9,27 @@ type TextInputProps = {
   }
 
 function TextInput({ name, label, type = 'text' }: TextInputProps) {
+  /* `name` is an array of information: `field` contains properties that
+  are "spread across" the input. `meta` contains metadata about the input
+  including error message defined in the schema. The equivalent would be
+  using useFormik() and manually spreading properties across the input:
+
+  const formik = useFormik()...
+  <input
+         id="firstName"
+         name="firstName"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.firstName}
+       />
+  */
   const [field, meta] = useField(name);
 
   return (
+
+    /* A Chakra UI element that has a `isInvalid` prop that, when true
+     displays an error message; these error messages are supplied by
+    the `meta` object. */
     <FormControl id={name} isInvalid={!!(meta.touched && meta.error)}>
       <FormLabel>{label}</FormLabel>
       <Input {...field} type={type} focusBorderColor="brand.600" />
