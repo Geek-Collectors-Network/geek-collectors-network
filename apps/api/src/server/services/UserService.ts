96a917dae7a007bf78e1b576596a8fb996efea20
Terence Grigoruk <terence.grigoruk@gmail.com>
2024-02-22 12:29:07 -0800

    feat(api): user profile info validation

--- apps/api/src/server/services/UserService.ts
+++ apps/api/src/server/services/UserService.ts
@@ -1,10 +1,27 @@
 import { eq } from 'drizzle-orm';
-import { MySqlInsertValue } from 'drizzle-orm/mysql-core';
-import { isSqlError } from '../utils';
 
 import { BaseService, type Resources } from './Service';
 import { user } from '../../models/user';
 
+import { z, ZodError } from 'zod';
+
+type ProfileData = {
+  email?: string
+  firstName?: string
+  lastName?: string
+  displayName?: string
+  profileImageUrl?: string
+  birthDate?: Date
+};
+
+const updateUserProfileSchema = z.object({
+  email: z.string().email().toLowerCase(),
+  firstName: z.string().max(20),
+  lastName: z.string().max(20),
+  displayName: z.string().max(20),
+  profileImageUrl: z.string().url().max(255),
+  birthDate: z.coerce.date(),
+}).partial();
 
 export class UserController {
   // eslint-disable-next-line no-useless-constructor
@@ -12,33 +29,31 @@ export class UserController {
 
   public async getProfile(id: number) {
     const results = await this.resources.db
-      .select()
+      .select({
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        displayName: user.displayName,
+        profileImageUrl: user.profileImageUrl,
+        birthDate: user.birthDate,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+        lastLoginAt: user.lastLoginAt,
+      })
       .from(user)
       .where(eq(user.id, id));
-
-    console.log(results);
-    if (results.length !== 1) {
-      return null;
-    }
-
-    // TODO: remove secret fields from result properly
-    results[0].hashedPassword = '';
-    results[0].salt = '';
-
-    return results[0];
+    return results.length !== 1 ? null : results[0];
   }
 
-  public async updateProfile(id: number, profileData: any) {
-    // TODO: validate and format profileData
-    const updateData = { displayName: profileData.displayName || 'no-name' };
-
+  public async updateProfile(id: number, updateData: ProfileData) {
     const results = await this.resources.db
       .update(user)
       .set(updateData)
       .where(eq(user.id, id));
 
-    // TODO: return fields that were changed (?)
-    return results[0].changedRows === 1;
+    // affectedRows will be 1 even if no data was actually changed;
+    // changedRows would show actual db changes (but is deprecated)
+    return { updated: results[0].affectedRows === 1 };
   }
 }
 
@@ -52,13 +67,18 @@ export class UserService extends BaseService {
     // TODO: use middleware to check authentication
 
     this.router.get('/profile', async (req, res) => {
+      // TODO: use query param to get profile of specified user (if friend)
       const id  = req.session.userId;
       if (!id) {
         res.status(401).json({ error: 'Not authenticated' });
         return;
       }
-      const profileData = await controller.getProfile(id);
-      res.status(200).json(profileData);
+      const getProfileResult = await controller.getProfile(id);
+      if (getProfileResult) {
+        res.status(200).json(getProfileResult);
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
     });
 
     this.router.patch('/profile', async (req, res) => {
@@ -67,9 +87,17 @@ export class UserService extends BaseService {
         res.status(401).json({ error: 'Not authenticated' });
         return;
       }
-      console.log(req.body);
-      const profileData = await controller.updateProfile(id, req.body);
-      res.status(200).json(profileData);
+      try {
+        const parsedUpdateData: ProfileData = updateUserProfileSchema.parse(req.body);
+        const updateProfileResult = await controller.updateProfile(id, parsedUpdateData);
+        res.status(200).json(updateProfileResult);
+      } catch (err) {
+        if (err instanceof ZodError) {
+          res.status(400).json({ errors: err.errors });
+        } else {
+          res.status(500).json({ error: 'Internal Server Error' });
+        }
+      }
     });
   }
 }
