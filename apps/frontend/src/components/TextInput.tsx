f69caf543cb7f8039861d88653739afbe61c43d7
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-02-23 14:01:38 -0800

    docs(frontend): remove comment from `TextInput`

--- apps/frontend/src/components/TextInput.tsx
+++ apps/frontend/src/components/TextInput.tsx
@@ -9,20 +9,6 @@ type TextInputProps = {
   }
 
 function TextInput({ name, label, type = 'text' }: TextInputProps) {
-  /* `name` is an array of information: `field` contains properties that
-  are "spread across" the input. `meta` contains metadata about the input
-  including error message defined in the schema. The equivalent would be
-  using useFormik() and manually spreading properties across the input:
-
-  const formik = useFormik()...
-  <input
-         id="firstName"
-         name="firstName"
-         type="text"
-         onChange={formik.handleChange}
-         value={formik.values.firstName}
-       />
-  */
   const [field, meta] = useField(name);
 
   return (
