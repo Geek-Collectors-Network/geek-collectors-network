b0a2fc7d907d1970209ad851354f24e6799e3bd2
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-03-13 12:53:52 -0700

    docs(frontend): update code comments

--- apps/frontend/src/components/SearchBar.tsx
+++ apps/frontend/src/components/SearchBar.tsx
@@ -3,9 +3,8 @@ import { Input } from '@chakra-ui/react';
 
 type SearchBarProps = {
 
-    /* `onSearch` is a function that takes a string as an argument and
-    returns nothing. This function will be used to filter the data
-    displayed in the table. */
+    /* This function will filter data to be displayed in parent component.
+    Ex. Displaying names containing the characters typed in by the user.  */
     onSearch: (search: string) => void;
 }
 
@@ -13,13 +12,6 @@ function SearchBar({ onSearch }: SearchBarProps) {
   return (
     <Input
       placeholder="Search..."
-
-      /* `onChange` listens for any changes inside Input, i.e., if the
-      user typed or deleted values; each time there's a change, this
-      function fires, which in turn will call the `onSearch` function.
-      `e`, the event, returns lots of info; `target` refers to the
-      HTML element, `Input`, and `target` specifically refers to the
-      characters in the input box; it's these that'll be used for search. */
       onChange={e => onSearch(e.target.value)}/>
   );
 }
